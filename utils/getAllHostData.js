8d6487e3faeb1267932e12ceeb0023b3a4740efd
Bradley Knutson <bradley@bknutson.com>
2022-08-02 16:45:56 -0600

    Modified getAllHostData to fit new server format

--- utils/getAllHostData.js
+++ utils/getAllHostData.js
@@ -1,24 +1,8 @@
 // @ts-nocheck
 /** @param {NS} ns */
-import {
-  servers0Port,
-  servers1Port,
-  servers2Port,
-  servers3Port,
-  servers4Port,
-  servers5Port,
-} from "/startup/servers";
+import { servers as allServers } from "/utils/allServers";
 
 export async function main(ns) {
-  const allServers = [
-    ...servers0Port,
-    ...servers1Port,
-    ...servers2Port,
-    ...servers3Port,
-    ...servers4Port,
-    ...servers5Port,
-  ];
-
   for (let server of allServers) {
     ns.getServerMaxMoney(server.host);
     ns.getServerRequiredHackingLevel(server.host);
